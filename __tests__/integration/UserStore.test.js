42aa3d8796a0312f074b443061add1fe5601ca3e
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2020-01-25 04:56:56 -0300

    test: validação de usuários

--- __tests__/integration/UserStore.test.js
+++ __tests__/integration/UserStore.test.js
@@ -13,7 +13,7 @@ describe('User Store', () => {
     const user = await factory.attrs('User');
     const { status, body } = await request(app)
       .post('/users')
-      .send(user);
+      .send({ ...user, passwordConfirm: user.password });
 
     expect(status).toBe(200);
     expect(body).toHaveProperty('name', user.name);
@@ -25,9 +25,41 @@ describe('User Store', () => {
     const user = await factory.create('User');
     const { status, body } = await request(app)
       .post('/users')
-      .send(user);
+      .send({
+        name: 'batata',
+        email: user.email,
+        password: '123456',
+        passwordConfirm: '123456',
+      });
 
     expect(status).toBe(400);
     expect(body).toHaveProperty('message');
+  });
+
+  it('Should be not able to store an User with invalid data', async () => {
+    const { status, body } = await request(app)
+      .post('/users')
+      .send({
+        name: '',
+        password: true,
+        passwordConfirm: false,
+        email: 8784514545,
+      });
+
+    expect(status).toBe(400);
+    expect(body).toHaveProperty('error');
+    expect(body).toHaveProperty('messages');
+  });
+
+  it('Should be not able to store an User without passwordConfirm', async () => {
+    const user = await factory.attrs('User');
+
+    const { status, body } = await request(app)
+      .post('/users')
+      .send(user);
+
+    expect(status).toBe(400);
+    expect(body).toHaveProperty('error');
+    expect(body).toHaveProperty('messages');
   });
 });
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ import { Router } from 'express';
 
 import UserController from './app/Controllers/UserController';
 
+import UserStoreValidator from './app/Validators/UserStoreValidator';
+
 const routes = new Router();
 
-routes.post('/users', UserController.store);
+routes.post('/users', UserStoreValidator, UserController.store);
 
 export default routes;
